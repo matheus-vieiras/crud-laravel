b8e8d1e59601d8e688787283ad2cd7564070299d
Matheus Vieira <matheusvieira.contato@hotmail.com>
2022-08-26 15:08:48 -0300

    Cache com arquivos 01

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,16 +10,32 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $users = User::with('posts')->paginate(5);
-
-
-
+//        $users = User::with('posts')->paginate(5);
 
 //        $post = Post::with(['user' => function ($query) {
 //            $query->select('id', 'firstName', 'lastName', 'email');
 //        }]);
 
 //        dd($post);
+
+//        cache()->flush(); ### exclui todos os caches
+
+        $users = cache()->rememberForever('users1', function () {
+            return User::limit(10)->get();
+        });
+
+
+//        if (cache()->has('users')) {
+//            $users = cache()->get('users');
+//        } else {
+//            $users = User::limit(10)->get();
+//            cache()->put('users', $users, now()->addMinute(10));
+//        }
+//
+//            cache()->forget('users');
+//
+//        dd($users);
+        dd(cache()->get('users'));
 
         return view('home', [
             'title' => 'Home crud',
