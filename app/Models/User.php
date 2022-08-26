3f4e43e94b786c4cc0bd1d74b7655406b6981b36
Matheus Vieira <matheusvieira.contato@hotmail.com>
2022-08-26 14:36:59 -0300

    Laravel Breeze #02

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Notifications\PasswordResetNotificationMessage;
+use Illuminate\Auth\Notifications\ResetPassword as ResetPasswordNotification;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -65,5 +67,10 @@ class User extends Authenticatable
 
     public function posts(){
         return $this->hasMany(Post::class,'user_id');
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new PasswordResetNotificationMessage($token));
     }
 }
